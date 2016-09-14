85b859ad4f321adcc4031f2927aa9f68482c31ce
Matias G. Sticchi <matgs656@gmail.com>
2016-09-13 21:10:24 -0300

    Create store.sh

---
+++ store.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+
+# Almacenamiento local recurso de video RTSP FLV ffmpeg
+
+VBR="400k"         # Bitrate del video
+FPS="30"           # FPS del video
+QUAL="medium"      # Preset de calidad FFMPEG
+
+SOURCE="rtsp://www.sitio.net:1935/live"              # Source UDP (voir les annonces SAP)
+
+ffmpeg \
+    -i "$SOURCE" -deinterlace \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -framerate $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 256k -bufsize 6000k file.flv \
