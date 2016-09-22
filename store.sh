0fafcb42dba35d3a8b0b7324e7bb8fde8b0302d2
Matias G. Sticchi <matgs656@gmail.com>
2016-09-22 09:47:18 -0300

    Refactor output file name in store.sh

--- store.sh
+++ store.sh
@@ -5,10 +5,12 @@
 VBR="400k"         # Bitrate del video
 FPS="30"           # FPS del video
 QUAL="medium"      # Preset de calidad FFMPEG
+DATE=`date +%d-%m-%Y`
+OUTPUT="$DATE.flv"
 
 SOURCE="rtsp://www.stream.net:0000/live"              # Source UDP (voir les annonces SAP)
 
 ffmpeg \
     -i "$SOURCE" -deinterlace \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -framerate $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 256k -bufsize 6000k file.flv \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 256k -bufsize 6000k $OUTPUT \
